cd2c59c4536df3e51e745681a5d4981914b43ecf
millagoss <millagoss19@gmail.com>
2025-11-09 23:36:16 +0300

    feat(api): axios client with env baseURL and response error logging

---
+++ src/lib/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL =
+	process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "")
+
+export const api = axios.create({
+	baseURL,
+	timeout: 15000,
+	headers: { "Content-Type": "application/json" },
+	withCredentials: false,
+});
+
+api.interceptors.request.use((config) => {
+	return config;
+});
+
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (process.env.NODE_ENV !== "production") {
+			// eslint-disable-next-line no-console
+			console.error("API Error:", error?.response?.status, error?.message);
+		}
+		return Promise.reject(error);
+	},
+);
+
+
--- src/store/Provider.tsx
+++ src/store/Provider.tsx
@@ -1,6 +1,6 @@
-\"use client\";
-import { Provider } from \"react-redux\";
-import { store } from \"./index\";
+"use client";
+import { Provider } from "react-redux";
+import { store } from "./index";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return <Provider store={store}>{children}</Provider>;
